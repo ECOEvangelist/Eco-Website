c5d7b7a3a412d56f2b575cd0e7ae33600ef0704e
Anestis Sitmalidis <asitmalidis@gmail.com>
2018-07-14 22:46:48 +0100

    Twig installed

--- config/bundles.php
+++ config/bundles.php
@@ -2,5 +2,9 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+<<<<<<< Updated upstream
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+=======
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+>>>>>>> Stashed changes
 ];
